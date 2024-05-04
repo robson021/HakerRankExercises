ffa721a55c5b988ec539a7504c0be9ddea5c08ec
Robert Nowak <robertn@amazon.com>
2024-05-04 20:52:43 +0200

    add String Comparison

---
+++ README.md
@@ -0,0 +1,5 @@
+Haker Rank exercises. Java 8 compatible code.
+
+### Run with:
+
+``./gradlew clean test``
---
+++ src/main/java/com/example/demo/SubstringComparison.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+// https://www.hackerrank.com/challenges/java-string-compare/problem
+
+class Solution {
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+
+        return smallest + "\n" + largest;
+    }
+
+}
