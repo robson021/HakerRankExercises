7144d502718813a30ffc9810e57f3ed4c9e36b22
Robert Nowak <robertn@amazon.com>
2024-05-06 19:16:21 +0200

    add Player Comparator

---
+++ src/main/java/com/example/demo/PlayerComparator.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+// https://www.hackerrank.com/challenges/java-comparator/problem
+class Player {
+    final String name;
+    final int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+public class PlayerComparator {
+    void doCompare(Player[] players) {
+        Arrays.sort(players, new Checker());
+        for (Player player : players) {
+            System.out.printf("%s %s\n", player.name, player.score);
+        }
+    }
+
+    private class Checker implements Comparator<Player> {
+        @Override
+        public int compare(Player p1, Player p2) {
+
+            if (p1.score == p2.score) {
+                return p1.name.compareTo(p2.name);
+            }
+
+            if (p1.score > p2.score) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+    }
+}
